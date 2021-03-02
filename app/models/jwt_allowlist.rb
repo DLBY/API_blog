21646c9202c33e5f25db71bfafc5ae0d28037999
David <david.lobry96@gmail.com>
2021-03-02 15:31:59 +0100

    add devise

---
+++ app/models/jwt_allowlist.rb
@@ -0,0 +1,2 @@
+class JwtAllowlist < ApplicationRecord
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.delete_all
+Article.delete_all
+puts'DB cleaned up successfully',
+
+User.create!(
+  email:'salutcestmoi@yopmail.com',
+  encrypted_password:'123456',
+)
+puts 'user created successfully'
+
+# generate 30 articles
+30.times do
+  Article.create!(
+    title:Faker::Book.unique.title,
+    content:Faker::TvShows::SouthPark.quote,
+    user_id: 6
+  )
+end
+puts '30 articles generated successfully'
+
